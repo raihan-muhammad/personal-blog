f0ccf214a69ff4033388d2ce2efa75b6edb2e99e
raihan-muhammad <sukasukangoding@gmail.com>
2023-08-17 16:06:55 +0700

    feat: add reading time and post time

--- components/post/post-content.tsx
+++ components/post/post-content.tsx
@@ -1,3 +1,4 @@
+import { getReadingTime, getRelativeDate } from "@/lib/helpers";
 import { Post } from "@/types/collection";
 import { ArrowRight } from "lucide-react";
 
@@ -14,9 +15,9 @@ const PostContent = ({ post }: PostContentProps) => {
                 <div className="w-2 h-2 rounded-full bg-neutral-200"/>
                 <div>{post.author.first_name} {post.author.last_name}</div>
                 <div className="w-2 h-2 rounded-full bg-neutral-200"/>
-                <div>1 min read</div>
+                <div>{getReadingTime(post.body)}</div>
                 <div className="w-2 h-2 rounded-full bg-neutral-200"/>
-                <div>1 week ago</div>
+                <div>{getRelativeDate(post.date_created)}</div>
             </div>
             <h2 className="font-medium text-3xl">{post.title}</h2>
             <p className="text-neutral-600">{post.description}</p>
