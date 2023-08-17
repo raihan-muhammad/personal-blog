4a8b248cb8e882238e153ecf0a8a729febdbfb21
raihan-muhammad <sukasukangoding@gmail.com>
2023-08-17 13:36:54 +0700

    feat: add site config information

---
+++ config/site.ts
@@ -0,0 +1,25 @@
+export interface SiteConfig {
+    siteName: string;
+    description: string;
+    currentlyAt: string;
+    socialLinks: {
+        youtube: string;
+        github: string;
+        linkedin: string;
+        instagram: string;
+    }
+}
+
+const siteConfig: SiteConfig = {
+    siteName: "Raihan Blog",
+    description: "A personal blog of me",
+    currentlyAt: 'Indonesia',
+    socialLinks: {
+        youtube: 'https://www.youtube.com/c/raihanMuhammad',
+        github: 'https://github.com/raihan-muhammad/',
+        linkedin: 'https://www.linkedin.com/in/raihan-muhammad/',
+        instagram: 'https://instagram.com/raihannmuhammad_'
+    }
+}
+
+export default siteConfig
